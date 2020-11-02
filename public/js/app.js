f461e86b3cf285c4d9e407c61586a445241ca9e4
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2020-11-01 21:08:43 -0300

    agregado timeout y habilitacion/desahabilitacion de inputs al grabar

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,34 @@
 $("#recButton").addClass("notRec");
-
 $(".botonRec").click(function () {
   if ($("#recButton").hasClass("notRec")) {
     const input_horas = $(".input-horas").val();
     const input_minutos = $(".input-minutos").val();
     const input_segundos = $(".input-segundos").val();
     if (evaluarCampos(input_horas, input_minutos, input_segundos)) {
-      grabar(input_horas, input_minutos, input_segundos);
-      //TODO desahilitar los inputs hasta que se deje de grabar
-      //TODO ver como conecto esto de lado server con el cliente
       $(".texto-grabacion").text("Grabando...");
       $("#recButton").removeClass("notRec");
       $("#recButton").addClass("Rec");
+      deshabilitarInputs();
+      iniciarCuentaRegresiva(input_horas, input_minutos, input_segundos);
+      grabar(input_horas, input_minutos, input_segundos);
+      //TODO desahilitar los inputs hasta que se deje de grabar
     }
   } else {
     //si ya estaba grabando
-
     $(".texto-grabacion").text("Iniciar Grabacion");
     $("#recButton").removeClass("Rec");
     $("#recButton").addClass("notRec");
   }
 });
 
+///limpio los inputs
 const limpiarCampos = () => {
   $(".input-horas").val("");
   $(".input-minutos").val("");
   $(".input-segundos").val("");
 };
 
+///POST -> envios h,m,s y en el server llamaré a funcion recorder
 const grabar = (input_horas, input_minutos, input_segundos) => {
   $.ajax({
     type: "POST",
@@ -49,6 +50,7 @@ const grabar = (input_horas, input_minutos, input_segundos) => {
 
 //TODO crear una peticion para que deje de grabar del lado servidor
 
+///comprobaciones para saber si los campos estan vacios o estan en un rango
 const evaluarCampos = (input_horas, input_minutos, input_segundos) => {
   let bandera = true;
 
@@ -79,4 +81,55 @@ const evaluarCampos = (input_horas, input_minutos, input_segundos) => {
   }
 
   return bandera;
+};
+
+///deshabilitar y habilitar input, util usarlo mientras grabo
+const deshabilitarInputs = () => {
+  $("input[type='number']").prop("disabled", true);
+};
+
+const habilitarInputs = () => {
+  $("input[type='number']").prop("disabled", false);
+};
+
+///cuenta regresiva segun tiempos puestos en los inputs
+const iniciarCuentaRegresiva = (input_horas, input_minutos, input_segundos) => {
+  ///hago que aparezca el div timeout
+  $("#timeout-text").removeClass("d-none");
+  $("#timeout-text").addClass("d-inline");
+
+  ///seteo una fucha futura
+  let dt = new Date();
+  dt.setHours(dt.getHours() + parseInt(input_horas));
+  dt.setMinutes(dt.getMinutes() + parseInt(input_minutos));
+  dt.setSeconds(dt.getSeconds() + parseInt(input_segundos));
+
+  let x = setInterval(() => {
+    ///fecha actual
+    const ahora = new Date().getTime();
+    ///resto fecha futura menos la actual
+    let dif = dt - ahora;
+
+    let horas = Math.floor((dif % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    horas = horas < 10 ? "0" + String(horas) : horas;
+    let minutos = Math.floor((dif % (1000 * 60 * 60)) / (1000 * 60));
+    minutos = minutos < 10 ? "0" + String(minutos) : minutos;
+    let segundos = Math.floor((dif % (1000 * 60)) / 1000);
+    segundos = segundos < 10 ? "0" + String(segundos) : segundos;
+
+    //muestro
+    $("#timeout-text").text(`${horas}:${minutos}:${segundos}`);
+
+    if (dif < 0) {
+      clearInterval(x);
+      //habilito input y hago desaparecer el texto de timeout
+      habilitarInputs();
+      $("#timeout-text").removeClass("d-inline");
+      $("#timeout-text").addClass("d-none");
+      //vuelvo al estado anterior del boton
+      $(".texto-grabacion").text("Iniciar Grabacion");
+      $("#recButton").removeClass("Rec");
+      $("#recButton").addClass("notRec");
+    }
+  }, 1000);
 };
