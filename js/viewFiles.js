5c7f8914a75121fb76285c29e50dc44c51f60299
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2020-11-02 23:38:29 -0300

    cambiada ubicacion de carpeta 'records' y 'snapshots'

--- js/viewFiles.js
+++ js/viewFiles.js
@@ -1,19 +1,24 @@
 const fs = require("fs");
+const path = require("path");
 
 const getListaArchivos = (tipo) => {
   if (tipo === 1) {
     ///grabaciones
     let archivos = [];
-    fs.readdirSync("./records/").forEach((file) => {
-      archivos.push(file);
-    });
+    fs.readdirSync(path.resolve(__dirname + "/../public/records")).forEach(
+      (file) => {
+        archivos.push(file);
+      }
+    );
     return archivos;
   } else if (tipo === 2) {
     ///capturas
     let archivos = [];
-    fs.readdirSync("./snapshots/").forEach((file) => {
-      archivos.push(file);
-    });
+    fs.readdirSync(path.resolve(__dirname + "/../public/snapshots")).forEach(
+      (file) => {
+        archivos.push(file);
+      }
+    );
     return archivos;
   }
 };
